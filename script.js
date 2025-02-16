fb7d33365d6c06e1f22838742d107041820d44c8
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-16 07:58:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-document,getElementById('frame').addEventListener('blur', function(){
-	this.vslue = this.value.toUpperCase();
+document.getElementById('fname').addEventListener('blur', function() {
+    this.value = this.value.toUpperCase();
 });
